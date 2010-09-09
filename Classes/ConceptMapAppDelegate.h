2c2768ad9860a54f7b6182ed2f68317453155209
Preston Rohner <prestonrohner@me.com>
2010-09-08 20:00:34 -0700

    first commit

---
+++ Classes/ConceptMapAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  ConceptMapAppDelegate.h
+//  ConceptMap
+//
+//  Created by Preston Rohner on 9/8/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ConceptMapViewController;
+
+@interface ConceptMapAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+    ConceptMapViewController *viewController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet ConceptMapViewController *viewController;
+
+@end
+
---
+++ Classes/ConceptMapViewController.h
@@ -0,0 +1,16 @@
+//
+//  ConceptMapViewController.h
+//  ConceptMap
+//
+//  Created by Preston Rohner on 9/8/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConceptMapViewController : UIViewController {
+
+}
+
+@end
+
