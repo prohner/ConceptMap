076347b8a7a2678b9dea0274be1f917de521acdd
Preston Rohner <prestonrohner@me.com>
2010-09-09 22:22:57 -0700

    basic test case added

---
+++ GeneralTests.h
@@ -0,0 +1,18 @@
+//
+//  GeneralTests.h
+//  ConceptMap
+//
+//  Created by Preston Rohner on 9/9/10.
+//  Copyright 2010 Cool Tool Apps. All rights reserved.
+//
+//  See Also: http://developer.apple.com/iphone/library/documentation/Xcode/Conceptual/iphone_development/135-Unit_Testing_Applications/unit_testing_applications.html
+
+#import "GTMSenTestCase.h"
+#import <UIKit/UIKit.h>
+
+
+@interface GeneralTests : GTMTestCase {
+
+}
+
+@end
---
+++ GeneralTests.m
@@ -0,0 +1,20 @@
+//
+//  GeneralTests.m
+//  ConceptMap
+//
+//  Created by Preston Rohner on 9/9/10.
+//  Copyright 2010 Cool Tool Apps. All rights reserved.
+//
+
+#import "GeneralTests.h"
+
+
+@implementation GeneralTests
+
+- (void) testMath {
+    
+    STAssertTrue((1+1)==2, @"Compiler isn't feeling well today :-(" );
+    
+}
+
+@end
