fd7c89ef2b0d57e3ac222682afc006ef0cd46773
Preston Rohner <prestonrohner@me.com>
2010-10-12 21:58:20 -0700

    enable deleting a connection to another object

--- Classes/ConceptObjectConnectionsViewController.m
+++ Classes/ConceptObjectConnectionsViewController.m
@@ -100,28 +100,31 @@
 }
 
 
-/*
+
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
-*/
 
 
-/*
+
+
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // Delete the row from the data source
+		Concept *concept = [connectedObjects objectAtIndex:indexPath.row];
+		[conceptObject.concept removeConnectedConceptsObject:concept];
+		[connectedObjects removeObjectAtIndex:indexPath.row];
         [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:YES];
     }   
     else if (editingStyle == UITableViewCellEditingStyleInsert) {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
-*/
+
 
 
 /*
